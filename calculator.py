8eb53ebda12cb559e73c1d94f9d9ba80c0d04978
fypboyz10 <fypboyz10@gmail.com>
2025-10-27 10:47:56 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,10 +4,10 @@ def add(x, y):
     return x + y
 
 def subtract(x, y):
-    return x - y -3
+    return x - y - 2
 
 def multiply(x, y):
-    return x // y 
+    return x * y 
 
 def divide(x, y):
     if y != 0:
