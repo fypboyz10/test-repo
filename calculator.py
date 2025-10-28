f2876d06ad5103d0f49ffbd62ace9508a2db7b28
fypboyz10 <fypboyz10@gmail.com>
2025-10-28 22:09:58 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -28,7 +28,8 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 
-choice = input("Enter choice (1/2/3/4): ")
+
+choice = input("Enter choice (1/2/3/4): "
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
