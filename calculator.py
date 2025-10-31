9042d0bfd7f14229fa189481dbb7f1e5aef907f2
fypboyz10 <fypboyz10@gmail.com>
2025-10-31 12:40:45 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,17 @@
 # Simple Python Calculator (Improved)
 
 def add(x, y):
-
-def subtract(x, y):
     return x + y
+    
+def subtract(x, y):
+    return x - y
 
 def multiply(x, y):
     return x * y
 
 def divide(x, y):
+    if y == 0:
+        return "Error: Cannot divide by zero"
     return x / y
 
 while True:
