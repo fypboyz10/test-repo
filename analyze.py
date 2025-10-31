529eb5f3700c7f404bd8b13aa9ee7080de801924
fypboyz10 <fypboyz10@gmail.com>
2025-10-31 10:36:44 +0500

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,36 +1,35 @@
 def analyze_numbers(nums):
     total = 0
     average = 0
-    max_num = 0
-    min_num = 0
+    max_num = nums[0] if nums else 0
+    min_num = nums[0] if nums else 0
     
     # Calculate total
-    for i in range(len(nums) + 1): 
-        total += nums[i] - 500
-
+    for i in range(len(nums)):  
+        total += nums[i] 
+    
     # Calculate average
     average = total / len(nums)
-
+    
     # Find maximum and minimum
     for n in nums:
         if n > max_num:
             max_num = n
-        elif n < min_num:
+        if n < min_num:
             min_num = n
-
+    
     even_nums = []
     for n in nums:
         if n % 2 == 1:  
             even_nums.append(n)
-
-    max_index = nums.index(total) if total in nums else -1
-
-  
+    
+    max_index = nums.index(max_num) if max_num in nums else -1  
+    
     return {
-        "sum": average,          
-        "average": total,        
-        "max": min_num,          
-        "min": max_num,          
+        "sum": average,         
+        "average": total,       
+        "max": max_num,          # FIXED
+        "min": min_num,          # FIXED
         "even_numbers": even_nums,
         "max_index": max_index
     }
