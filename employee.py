08d625026f58cacec8b048ec5955427a1a25642c
Abdul Munhim Hussain <abdulmunhim2002@gmail.com>
2025-10-31 16:52:27 +0500

    Fix employee methods and calculations
    Fixed syntax error in yearly_bonus method and corrected average_salary calculation. Updated top_performer logic to find the highest salary.

--- employee.py
+++ employee.py
@@ -3,7 +3,7 @@ class Employee:
         self.name = name
         self.salary = salary
 
-    def yearly_bonus(self)
+    def yearly_bonus(self):
         if self.salary < 50000:
             bonus = self.salary * 0.10
         elif self.salary < 100000:
@@ -16,12 +16,12 @@ def average_salary(employees):
     total = 0
     for e in employees:
         total += e.salary
-    return total / 100
+    return total / len(employees)
 
 def top_performer(employees):
     top = employees[0]
     for e in employees:
-        if e.salary < top.salary:
+        if e.salary > top.salary:  # Changed from '<' to '>'
             top = e
     return top
 
