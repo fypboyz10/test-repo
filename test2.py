5ce12524a17ee9a614175f7b7949cf7365552e1c
hamzaimran28 <114745265+hamzaimran28@users.noreply.github.com>
2025-12-09 22:36:00 +0500

    Add test2.py with initial function definitions

---
+++ test2.py
@@ -0,0 +1,15 @@
+import os,sys, json
+import numpy as  np
+import math
+import math as m
+
+CONSTANTvalue = 10
+someVar = 5
+
+def Myfunction():
+    tempVar = 123
+    unused_var = 99
+    return tempVar
+
+def anotherFunction():
+    pass
