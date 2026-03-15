c892925ffefb9405120a08c729e9478de90a2125
fypboyz10 <fypboyz10@gmail.com>
2026-03-16 00:36:23 +0500

    Update style.py

--- style.py
+++ style.py
@@ -1,4 +1,15 @@
-def calculateTotalPrice(itemprice,TaxRate):
- total=itemprice+itemprice*TaxRate
- if(total>100):print("Large order")
- return total
+import os,sys
+
+def DoStuff( X,Y ):
+  result=X+Y
+  if(result>10):print("big result");a=1
+  else:
+   print( "small result" )
+  for  i in range(0,5):print(i)
+  veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongVariableName=result
+  return  veryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongVariableName
+
+class  myclass:
+ def __init__( self ,Name ):
+  self.Name=Name
+ def PrintName(self):print("Name is:",self.Name)
