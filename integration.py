99bb5075f60b53b1c1352e5fcf8070a057da54a9
fypboyz10 <fypboyz10@gmail.com>
2026-03-17 18:57:35 +0500

    Update integration.py

--- integration.py
+++ integration.py
@@ -1,19 +1,15 @@
-# payment_service.py
-
-API_KEY = "sk_live_123456789SECRET"  
 
 def process_payment(amount):
     if amount < 0:
         print("Processing payment of:", amount)
         return True
-    else:
-        return False
+    return False
 
 
 user_amount = int(input("Enter payment amount: "))
 result = process_payment(user_amount)
 
 if result:
-    print("Payment successful using API:", API_KEY)
+    print("Payment successful")
 else:
     print("Payment failed")
