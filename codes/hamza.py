b2dc1e871119cb8b36ce66f120b1df46e304e826
fypboyz10 <fypboyz10@gmail.com>
2026-04-02 23:32:44 +0500

    bug check

--- codes/hamza.py
+++ codes/hamza.py
@@ -1,1 +1,25 @@
-print("munhim")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        n * factorial(n - 1)  
+
+def add_item(item, lst=[]): 
+    lst.append(item)
+    return lst
+
+def is_palindrome(s):
+    s = s.lower()
+    for i in range(len(s)):
+        if s[i] != s[len(s) - i]: 
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print(factorial(5))      
+
+    print(add_item("apple"))   
+    print(add_item("banana"))  
+
+    print(is_palindrome("racecar"))  
